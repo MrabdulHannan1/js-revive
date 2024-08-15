dcab85b0690025afc9ba11bebe53b41a3ded519e
abdul hannan <hannanmughal007@gmail.com>
2024-08-15 19:43:11 +0500

    js/react revived

---
+++ ptc.js
@@ -0,0 +1,118 @@
+// // count number of strings
+
+// // const count={};
+// // function countchar(str){
+// //     for (let i=0;i<str.length;i++){
+// //     const char=str[i]
+
+// //     if (count[char])
+    
+// //     {
+// // count[char]++;
+// //     }
+// //     else {
+// // count[char]=1
+// //     }
+// //     }
+// //     return count;
+// // }
+
+// // const name ="hanan" ;
+// // const testcount=countchar(name)
+// // console.log(testcount);
+
+// //check if number is palindrome or not
+
+// let num= 1213;
+// let reversenum=0;
+// let temp=num;
+// let digit;
+// while (temp!==0){
+//     digit=temp%10;
+//     reversenum=(reversenum*10)+digit;
+//     temp=Math.floor(temp/10);
+
+// }
+// if(num===reversenum)
+//     console.log("number is palindrome");
+// else
+// console.log("number is not palindrome");
+
+
+
+// let str="abdul hannan"
+// let count={};
+// function strcount(str){
+// for (let i=0;i<str.length;i++)
+//     {
+//         const char=str[i];
+//         if (count[char]){
+//             count[char]++;
+//         }
+//         else
+//         count[char]=1
+// }
+// return count;
+// }
+// console.log(
+//         strcount(str)
+// )
+
+////////////objects///////////
+
+// var person={
+//     name:'ali',
+// age:456,
+// gender:'male'};
+
+// // console.log(person.name)
+
+// // //object destructuring
+
+// // const {name,age,gender} =person
+
+// // console.log(gender)
+
+
+// const hannan= person.keys();
+// 
+
+// let arr=[1,2,3,45,4,5,6,5,7]
+
+// // let arr2=["hanann","hanajkn","han5nn"]
+// //  const ar3 =[...arr,...arr2];
+// //let count={};
+
+// // for (let i=0;i<arr.length;i++)
+// // {
+// //     const num=arr[i];
+// //     if(count[num]=!count){
+// //         count[num]++;
+// //     }
+// // }
+// arr.push("hannan")
+//  console.log(arr)
+
+
+// console.log(arr.length) 
+
+// let person = {
+//     name: 'John',
+//     age: 30,
+//     greet: function() {
+//     console.log("Hello, my name is " + this.name + " ");
+//     }
+//     };
+//     person.greet(); // Output: Hello, my name is John.
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(2,3))
+
+// const sum =(a,b)=>a+b;
+// console.log(sum(5,6))
+
+const greet=()=>console.log("hello arow")
+greet()
